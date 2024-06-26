c1e8e8504e70338c1260a1e448bd5cfb72b9952e
Sumedh-C <sumedhchaware5269@gmail.com>
2024-06-26 19:55:13 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,21 @@
+package springAutoWired;
+
+public class Department 
+{
+	private int departmntId;
+	private String DepartmentRoll;
+	
+	public Department() {
+		super();
+	}
+	public Department(int departmntId, String departmentRoll) {
+		super();
+		this.departmntId = departmntId;
+		DepartmentRoll = departmentRoll;
+	}
+	@Override
+	public String toString() {
+		return "Department [departmntId=" + departmntId + ", DepartmentRoll=" + DepartmentRoll + "]";
+	}
+	
+}
---
+++ Employee.java
@@ -0,0 +1,30 @@
+package springAutoWired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee 
+{
+	private int empId;
+	private String empName;
+	private String empRoll;
+	private int empSalary;
+	
+	 @Autowired
+       private	Department department;
+	public Employee(int empId, String empName, String empRoll, int empSalary, Department department) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empRoll = empRoll;
+		this.empSalary = empSalary;
+		this.department = department;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empRoll=" + empRoll + ", empSalary=" + empSalary
+				+ ", department=" + department + "]";
+	}
+	
+	
+	
+}
